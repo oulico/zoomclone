e5b27933d939bd22dab3a2212a15e1bf9ff28a77
JH Lee <13332150+oulico@users.noreply.github.com>
2023-11-02 11:13:58 +0900

    FEAT: send offer after make connection

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,10 +4,15 @@ const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras")
+const call = document.getElementById("call");
+
+call.hidden = true;
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
+let myPeerConnection;
 
 async function getCameras() {
     try {
@@ -58,8 +63,6 @@ async function getMedia(deviceId) {
     }
 }
 
-getMedia()
-
 
 function handleMuteClick() {
     myStream.getAudioTracks().forEach((track) => {
@@ -91,6 +94,50 @@ async function handleCameraChange() {
     await getMedia(camerasSelect.value)
 }
 
+// Welcome Form (join a room)
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+async function startMedia() {
+    welcome.hidden = true;
+    call.hidden = false;
+    await getMedia();
+    makeConnection();
+}
+
+function handleWelcomeSubmit(event) {
+    event.preventDefault();
+    const input = welcome.querySelector("input");
+    socket.emit("join_room", input.value, startMedia);
+    roomName = input.value;
+    input.value = "";
+}
+
 muteBtn.addEventListener("click", handleMuteClick)
 cameraBtn.addEventListener("click", handleCameraClick)
 camerasSelect.addEventListener("input", handleCameraChange)
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+//Socket Code
+socket.on("welcome", async () => {
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer);
+    console.log("sent the offer")
+    socket.emit("offer", offer, roomName);
+})
+
+socket.on("offer", (offer) => {
+    console.log(offer);
+})
+
+// RTC Code
+
+function makeConnection() {
+    myPeerConnection = new RTCPeerConnection();
+    // peer to peer connection을 만들고,
+    myStream.getTracks().forEach((track) => {
+        myPeerConnection.addTrack(track, myStream);
+        // myStream의 track을 myPeerConnection에 추가한다.
+    })
+}
