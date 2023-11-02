c493f319ce6797746814a20213dc4a08638d8d53
JH Lee <13332150+oulico@users.noreply.github.com>
2023-11-02 10:21:14 +0900

    FEAT: 카메라 변경 기능

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import http from "http";
 import express from "express";
-import {instrument} from "@socket.io/admin-ui";
-import {Server} from "socket.io";
+import SocketIO from "socket.io";
 
 const app = express();
 
@@ -11,72 +10,8 @@ app.use("/public", express.static(__dirname + "/public"))
 app.get("/", (_, res) => res.render("home"))
 app.get("/*", (_, res) => res.redirect("/"))
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-
 const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer, {
-    cors: {
-        origin: ["https://admin.socket.io"],
-        credentials: true
-    },
-});
+const wsServer = SocketIO(httpServer);
 
-instrument(wsServer, {
-    auth: false,
-})
-
-function publicRooms() {
-    const {sockets: {adapter: {sids, rooms}}} = wsServer;
-    const publicRooms = [];
-    rooms.forEach((_, key) => {
-        if (sids.get(key) === undefined) {
-            publicRooms.push(key)
-        }
-    })
-    return publicRooms;
-}
-
-function countRoom(roomName) {
-    return wsServer.sockets.adapter.rooms.get(roomName)?.size
-}
-
-wsServer.on("connection", (socket) => {
-    socket["nickname"] = "익명의 유저";
-    socket.onAny((event) => {
-        console.log(`Socket event: ${event}`)
-        console.log('adapter 내용', wsServer.sockets.adapter)
-    })
-    socket.on("enter_room", (roomName, done) => {
-        socket.join(roomName);
-        done();
-        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-        wsServer.sockets.emit("room_change", publicRooms())
-    })
-
-    socket.on("disconnecting", () => {
-        socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1))
-    })
-
-    socket.on("disconnect", () => {
-        wsServer.sockets.emit("room_change", publicRooms())
-    })
-
-    socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", `${socket.nickname}:${msg}`);
-        done();
-    })
-
-    socket.on("nickname", (nickname) => {
-        socket["nickname"] = nickname;
-    })
-
-});
-
-function onSocketClose() {
-    console.log("close session")
-}
-
-const sockets = [];
-
-
-httpServer.listen(3000, handleListen)
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
